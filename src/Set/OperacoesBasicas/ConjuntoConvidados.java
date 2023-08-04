d84a778e3ce093d756a54b2e155e7e768d2f3cf0
Pedro-HFelix <pf711363@gmail.com>
2023-08-04 15:19:58 -0300

    testes de Operações Básicas

--- src/Set/OperacoesBasicas/ConjuntoConvidados.java
+++ src/Set/OperacoesBasicas/ConjuntoConvidados.java
@@ -29,4 +29,26 @@ public class ConjuntoConvidados {
     public void exibirConvidados(){
         System.out.println(convidadoSet);
     }
+    public static void main(String[] args) {
+        ConjuntoConvidados conjuntoConvidados = new ConjuntoConvidados();
+        System.out.println("Testando contarConvidados");
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() +" convidado(s) dentro do set de convidados");
+        System.out.println("----------------------------");
+        System.out.println("Testando adicionarConvidados");
+        conjuntoConvidados.adicionarConvidado("Convidado 1",1234);
+        conjuntoConvidados.adicionarConvidado("Convidado 2",1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 3",1235);
+        conjuntoConvidados.adicionarConvidado("Convidado 4",1236);
+        System.out.println("Existem " + conjuntoConvidados.contarConvidados() +" convidado(s) dentro do set de convidados");
+        System.out.println("-------------------------------------------");
+        System.out.println("Testando o exibir convidados");
+        conjuntoConvidados.exibirConvidados();
+        System.out.println("---------------------------");
+        System.out.println("Testando removerConvidados");
+        conjuntoConvidados.removerConvidadoPorCodigoConvite(1234);
+        System.out.println("O número total de convidados é: " +conjuntoConvidados.contarConvidados() );
+        conjuntoConvidados.exibirConvidados();
+    }
 }
+
+
--- src/Set/OperacoesBasicas/Convidado.java
+++ src/Set/OperacoesBasicas/Convidado.java
@@ -1,5 +1,7 @@
 package Set.OperacoesBasicas;
 
+import java.util.Objects;
+
 public class Convidado {
     private String nome;
     private int codigoConvite;
@@ -17,6 +19,18 @@ public class Convidado {
         return codigoConvite;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Convidado convidado)) return false;
+        return getCodigoConvite() == convidado.getCodigoConvite();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigoConvite());
+    }
+    // esses dois metodos acima, servem para compar e não deixar ter um valor duplicado/repetido
     @Override
     public String toString() {
         return "Convidado{" +
